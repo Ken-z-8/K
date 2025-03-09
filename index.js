a7da1b22dd39d0129a08fefc215a742ab0c655f2
Ken-z-8 <forrkenhich@gmail.com>
2025-03-09 23:28:39 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("scroll", function () {
-    let parallaxElements = document.querySelectorAll(".parallax, .parallax-2");
-
-    parallaxElements.forEach(function (element) {
-        let scrollPosition = window.scrollY;
-        element.style.backgroundPosition = "center " + (scrollPosition * 0.5) + "px";
-    });
-});
